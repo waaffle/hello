769216b85f0c6bfe99cb9e791851ea87e2a393d5
waaffle <126258587+waaffle@users.noreply.github.com>
2023-02-24 21:02:18 +0300

    Add files via upload

---
+++ myfile.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void test(vector <int> &v,int l, int r)
+{
+	int i = l, j = r;
+	int m = ((i + j) / 2);
+	while (i + 1 < j)
+	{
+		test(v, i, m);
+		test(v, m, j);
+	}
+	v[i] = l;
+	v[j] = r;
+	return;
+}
+
+
+int main() {
+	int n;
+	cin >> n;
+	vector <int> v(n);
+	test(v, 0, n);
+	return 0;
+}
+
+
